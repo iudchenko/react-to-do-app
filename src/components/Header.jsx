8d0493dd26bc6f694547e843ccb2302e190a4fda
iudchenko <andrii.iudchenko@gmail.com>
2023-09-02 19:45:52 +0200

    wording of the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className="flex justify-between p-5 max-w-2xl mx-auto gap-2">
       <h1 className="text-xl md:text-3xl font-bold justify-center">
-        What are you up for today?
+        What are you up to today?
       </h1>
       <DarkModeToggle />
     </header>
